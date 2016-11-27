9a99e314dd8179a583d0e8d38092fa73051b043d
JamesNikolaidis <dimitriosnikolaidis@hotmail.com>
2016-11-27 16:47:09 +0200

    Final

---
+++ UiautomatorTest/app/src/androidTest/java/com/applicationforlife/jamesnikolaidis/uiautomatortest/ExampleInstrumentedTest.java
@@ -0,0 +1,97 @@
+package com.applicationforlife.jamesnikolaidis.uiautomatortest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiObject;
+import android.support.test.uiautomator.UiObjectNotFoundException;
+import android.support.test.uiautomator.UiSelector;
+import android.support.test.uiautomator.Until;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+
+public class ExampleInstrumentedTest {
+    private static final String BASIC_SAMPLE_PACKAGE
+            = "com.greenriders.app";
+    private static final int LAUNCH_TIMEOUT = 5000;
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = mDevice.getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)),
+                LAUNCH_TIMEOUT);
+
+        // Launch the app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(BASIC_SAMPLE_PACKAGE);
+        // Clear out any previous instances
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
+                LAUNCH_TIMEOUT);
+    }
+
+
+
+
+
+
+    @Test
+    public void CheckForFirstTwoEditText_and_Login_Button() throws UiObjectNotFoundException {
+        UiObject mEmailEditText = mDevice.findObject(new UiSelector().resourceId("com.greenriders.app:id/loginEmail").className("android.widget.EditText"));
+
+        UiObject mLoginButton = mDevice.findObject(new UiSelector().resourceId("com.greenriders.app:id/logIn").className("android.widget.Button"));
+
+
+
+        mEmailEditText.setText("testingemail@hotmail.com");
+
+        UiObject mPasswordEditText = mDevice.findObject(new UiSelector().resourceId("com.greenriders.app:id/loginPassword").className("android.widget.EditText"));
+        mPasswordEditText.click();
+        mPasswordEditText.setText("testing");
+        mLoginButton.click();
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
